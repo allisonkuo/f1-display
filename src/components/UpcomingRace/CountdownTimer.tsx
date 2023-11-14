bee2025d8f0eb0a4eb23ffae2d24483f347012e0
allisonkuo <aly.kuo@gmail.com>
2023-11-13 17:37:10 -0800

    Experimenting with the styling of the countdown timer

--- src/components/UpcomingRace/CountdownTimer.tsx
+++ src/components/UpcomingRace/CountdownTimer.tsx
@@ -3,7 +3,7 @@ import CountdownTimerElement from './CountdownTimerElement';
 
 const Countdown = (): React.ReactNode => {
   return (
-    <div className='flex space-x-6'>
+    <div className='flex space-x-3'>
       <CountdownTimerElement time='03' unit='days' />
       <CountdownTimerElement time='12' unit='hrs' />
       <CountdownTimerElement time='47' unit='min' />
--- src/components/UpcomingRace/CountdownTimerElement.tsx
+++ src/components/UpcomingRace/CountdownTimerElement.tsx
@@ -11,10 +11,14 @@ const CountdownTimerElement = ({
 }: CountdownTimerElementProps): React.ReactNode => {
   return (
     <div className='mt-2'>
-      <div className='flex w-20 h-20 bg-f1-teal rounded-3xl justify-center items-center mb-3'>
-        <p className='text-3xl font-bold text-white'>{time}</p>
+      <div className='flex w-[90px] h-[90px] bg-f1-teal rounded-3xl justify-center items-center m-1'>
+        <p className='text-5xl font-bold text-white'>{time}</p>
       </div>
-      <p className='text-lg uppercase text-white'>{unit}</p>
+
+      {/* <div className='flex w-20 h-20 justify-center items-center'>
+        <p className='text-7xl font-bold text-f1-teal'>{time}</p>
+      </div> */}
+      <p className='text-lg uppercase text-white tracking-widest'>{unit}</p>
     </div>
   );
 };
