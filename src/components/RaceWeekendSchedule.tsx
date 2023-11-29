a031a28aadae66c8c495c87e0abd9840785e57a6
allisonkuo <aly.kuo@gmail.com>
2023-11-29 15:48:09 -0800

    Use API data to populate rache schedule component

--- src/components/RaceWeekendSchedule.tsx
+++ src/components/RaceWeekendSchedule.tsx
@@ -1,25 +1,28 @@
-import { parse } from 'path';
 import React from 'react';
 
 type ScheduleLineProps = {
   event: string;
-  date: string;
-  time: string;
+  datetime: string;
 };
 
 const ScheduleLine = ({
   event,
-  date,
-  time,
+  datetime,
 }: ScheduleLineProps): React.ReactNode => {
   const fontWeight = event === 'RACE DAY' ? 'font-semibold' : 'font-regular';
   const fontColor = event === 'RACE DAY' ? 'text-f1-teal' : 'text-white';
 
+  const pstDate = utcToPst(datetime).split(', ');
+
   return (
     <div className='grid grid-cols-8'>
       <p className={`col-span-4 text-sm ${fontWeight} ${fontColor}`}>{event}</p>
-      <p className={`col-span-3 text-sm ${fontWeight} ${fontColor}`}>{date}</p>
-      <p className={`col-span-1 text-sm ${fontWeight} ${fontColor}`}>{time}</p>
+      <p className={`col-span-3 text-sm ${fontWeight} ${fontColor}`}>
+        {pstDate[0]}
+      </p>
+      <p className={`col-span-1 text-sm ${fontWeight} ${fontColor}`}>
+        {pstDate[1].replace(/(:\d{2}| [AP]M)$/, '')}
+      </p>
     </div>
   );
 };
@@ -34,26 +37,36 @@ const RaceWeekendSchedule = ({
   const parsedSchedule = JSON.parse(schedule);
   console.log(parsedSchedule);
 
-  // convert time to pst here?
-  const seshOne = parsedSchedule['Session1DateUtc'];
-  const pstDate = utcToPst(seshOne);
-  console.log(pstDate);
-
   return (
     <div className='h-full bg-f1-grey px-6 py-4 space-y-4'>
       <h3 className='font-display text-white text-lg'>Schedule</h3>
-      <ScheduleLine event='Free Practice 1' date='Fri, Oct 22' time='11:00' />
-      <ScheduleLine event='Free Practice 2' date='Fri, Oct 22' time='15:00' />
-      <ScheduleLine event='Free Practice 3' date='Sat, Oct 23' time='11:30' />
-      <ScheduleLine event='Qualifying' date='Sat, Oct 23' time='15:30' />
-      <ScheduleLine event='RACE DAY' date='Sun, Oct 24' time='13:00' />
+      <ScheduleLine
+        event='Free Practice 1'
+        datetime={parsedSchedule['Session1DateUtc']}
+      />
+      <ScheduleLine
+        event='Free Practice 2'
+        datetime={parsedSchedule['Session2DateUtc']}
+      />
+      <ScheduleLine
+        event='Free Practice 3'
+        datetime={parsedSchedule['Session3DateUtc']}
+      />
+      <ScheduleLine
+        event='Qualifying'
+        datetime={parsedSchedule['Session4DateUtc']}
+      />
+      <ScheduleLine
+        event='RACE DAY'
+        datetime={parsedSchedule['Session5DateUtc']}
+      />
     </div>
   );
 };
 
 const utcToPst = (epoch: string) => {
   const utcDate = new Date(epoch);
-  return utcDate.toLocaleString('en-US', {
+  return utcDate.toLocaleString('en-GB', {
     timeZone: 'America/Los_Angeles',
   });
 };
